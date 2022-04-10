be96a8cc3264f86bcb3c6313e2ee2b748fd94f02
Marciano Moreno <marciano_moreno@outlook.com>
2022-04-10 16:22:16 -0500

    Implemented getTasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.tasksService.getHello();
   }
+
+  @Get()
+  getTasks(): Task[] {
+    return this.tasksService.getTasks();
+  }
+
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
+  private tasks: Task[] = [ { id:0, name: 'Task 1' }, { id:1, name: 'Task 2' }, {id: 2, name: 'Task 3' } ];
+
   getHello(): string {
     return "Hello tasks!";
+  }
+
+  getTasks(): Task[] {
+    return this.tasks;
   }
 }
