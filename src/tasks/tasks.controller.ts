1ab27b8cfe144b75f7655ff716b6152d23e7fef6
Marciano Moreno <marciano_moreno@outlook.com>
2022-04-10 17:50:19 -0500

    Implemented create task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './entities/task.entity';
 import { TasksService } from './tasks.service';
 
@@ -15,5 +16,9 @@ export class TasksController {
     return this.tasksService.getTasks();
   }
 
-
+  @Post()
+  createTask(@Body() body: CreateTaskDto ): Task {
+    console.log("createTask", body);
+    return this.tasksService.createTask(body);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, Post } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [ { id:0, name: 'Task 1' }, { id:1, name: 'Task 2' }, {id: 2, name: 'Task 3' } ];
+  private tasks: Task[] = [ { id:1, name: 'Task 1' }, { id:2, name: 'Task 2' }, {id: 3, name: 'Task 3' } ];
 
   getHello(): string {
     return "Hello tasks!";
@@ -12,4 +13,11 @@ export class TasksService {
   getTasks(): Task[] {
     return this.tasks;
   }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const newTask: Task = {id: this.tasks.length + 1, ...createTaskDto};
+    this.tasks.push(newTask);
+    return newTask;
+  }
+
 }
